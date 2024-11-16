f8b57e95f86cfb868afad611a1106c5931ab105b
Denis <denispavlenko7610new@gmail.com>
2024-11-16 15:12:09 +0200

    comment not necessary methods in lifecycle

--- Runtime/LifeCycle/MonoRunner.cs
+++ Runtime/LifeCycle/MonoRunner.cs
@@ -15,53 +15,53 @@ namespace RD_Tween.Runtime.LifeCycle
             Initialize();
         }
 
-        private async void OnEnable()
-        {
-            BeforeEnable();
-            Appear();
-        }
+        // private async void OnEnable()
+        // {
+        //     BeforeEnable();
+        //     Appear();
+        // }
 
-        private async void Update()
-        {
-            BeforeUpdate();
-            Run();
-        }
-
-        private async void FixedUpdate()
-        {
-            BeforeFixedUpdate();
-            FixedRun();
-        }
-
-        private async void LateUpdate()
-        {
-            BeforeLateUpdate();
-            LateRun();
-        }
-
-        private async void OnDisable()
-        {
-            BeforeDisable();
-            Disappear();
-        }
-
-        private async void OnDestroy()
-        {
-            BeforeDestroy();    
-            Delete();
-        }
+        // private async void Update()
+        // {
+        //     BeforeUpdate();
+        //     Run();
+        // }
+        //
+        // private async void FixedUpdate()
+        // {
+        //     BeforeFixedUpdate();
+        //     FixedRun();
+        // }
+        //
+        // private async void LateUpdate()
+        // {
+        //     BeforeLateUpdate();
+        //     LateRun();
+        // }
+        //
+        // private async void OnDisable()
+        // {
+        //     BeforeDisable();
+        //     Disappear();
+        // }
+        //
+        // private async void OnDestroy()
+        // {
+        //     BeforeDestroy();    
+        //     Delete();
+        // }
 
         /// <summary>
         /// Use this methods only once on your entry point
         /// </summary>
         protected virtual void BeforeAwake() {}
         protected virtual void BeforeStart() {}
-        protected virtual void BeforeEnable() {}
-        protected virtual void BeforeDisable() {}
-        protected virtual void BeforeUpdate() {}
-        protected virtual void BeforeFixedUpdate() {}
-        protected virtual void BeforeLateUpdate() {}
-        protected virtual void BeforeDestroy() {}
+        //protected virtual void BeforeEnable() {}
+        // protected virtual void BeforeDisable() {}
+        // protected virtual void BeforeUpdate() {}
+        // protected virtual void BeforeFixedUpdate() {}
+        // protected virtual void BeforeLateUpdate() {}
+        // protected virtual void BeforeDestroy() {}
         
         
         /// <summary>
@@ -69,11 +69,11 @@ namespace RD_Tween.Runtime.LifeCycle
         /// </summary>
 
         protected virtual void Initialize() {}
-        protected virtual void Appear() {}
-        protected virtual void Run() {}
-        protected virtual void FixedRun() {}
-        protected virtual void LateRun() {}
-        protected virtual void Disappear() {}
-        protected virtual void Delete() {}
+        //protected virtual void Appear() {}
+        // protected virtual void Run() {}
+        // protected virtual void FixedRun() {}
+        // protected virtual void LateRun() {}
+        // protected virtual void Disappear() {}
+        // protected virtual void Delete() {}
     }
 }
